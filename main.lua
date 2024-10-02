1f1b9b2f9eca84b1585e036f5346265d2fa167cb
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-02 22:13:30 +0900

    Update and rename .gitignore to main.lua

---
+++ main.lua
@@ -0,0 +1,9 @@
+serverhop = true --If true, default serverhop. If false, small serverhop
+FarmSiphon = true --If true, detects siphon orb. If false, skips siphon orb
+FarmHonor = true --If true, detects honor orb. If false, skips honor orb
+FarmPhase = true --If true, detects phase orb. If false, skips phase orb
+FarmJet = true --If true, detects jet orb. If false, skips jet orb
+FarmGlitch = true --If true, detects glitch orb. If false, skips glitch orb
+FarmToolbox = true --If true, detects toolbox. If false, skips toolbox
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/autofindorb/main/main.lua'))()
