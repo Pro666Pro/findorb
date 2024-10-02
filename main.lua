1b06590f9ab16baeb10dd3f7c016437ecd182b81
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-02 22:20:15 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+--my first configurable script, i hope you like it :3
+
 serverhop = true --If true, default serverhop. If false, small serverhop
 FarmSiphon = true --If true, detects siphon orb. If false, skips siphon orb
 FarmHonor = true --If true, detects honor orb. If false, skips honor orb
@@ -5,5 +7,6 @@ FarmPhase = true --If true, detects phase orb. If false, skips phase orb
 FarmJet = true --If true, detects jet orb. If false, skips jet orb
 FarmGlitch = true --If true, detects glitch orb. If false, skips glitch orb
 FarmToolbox = true --If true, detects toolbox. If false, skips toolbox
+FarmSlapples = true --If true, collects Slapples. If false, skips slapples
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/autofindorb/main/main.lua'))()
